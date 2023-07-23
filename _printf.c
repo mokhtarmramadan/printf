85d2ed1db5d1250743f320d9202288b1c52c3feb
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2023-07-23 04:11:12 -0700

    Fix

--- _printf.c
+++ _printf.c
@@ -14,6 +14,8 @@ int _printf(const char *format, ...)
 	int i;
 	int number;
 	int c;
+	char *s;
+	int d;
 
 	va_start(ap, format);
 	i = 0;
@@ -30,8 +32,18 @@ int _printf(const char *format, ...)
 			}
 			else if (format[i] == 's')
 			{
-				number += print_string(va_arg(ap, char *), number);
-				
+				s = va_arg(ap, char *);
+				if (s == NULL)
+				{
+					s = "(null)";
+				}
+				d = 0;
+				while (s[d] != '\0')
+				{
+
+					number += write(1, &s[d], 1);
+					d++;
+				}
 			}
 			else if (format[i] == '%')
 			{
