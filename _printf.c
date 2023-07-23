344e17fb252cde0c4d192a6c261adc3b08f3225a
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2023-07-23 04:42:29 -0700

    Fix

--- _printf.c
+++ _printf.c
@@ -1,4 +1,5 @@
 #include <stdarg.h>
+#include <stdlib.h>
 #include <string.h>
 #include "main.h"
 #include <unistd.h>
@@ -16,43 +17,74 @@ int _printf(const char *format, ...)
 	int c;
 	char *s;
 	int d;
+	int integer;
+	int div;
+	char digit;
 
 	va_start(ap, format);
 	i = 0;
 	number = 0;
+	integer = 0;
 	while (format && format[i])
 	{
 		if (format[i] == '%')
 		{
 			i++;
-			if (format[i] == 'c')
+			switch(format[i])
 			{
-				c = va_arg(ap, int);
-				number += write(1, &c, 1);
-			}
-			else if (format[i] == 's')
-			{
-				s = va_arg(ap, char *);
-				if (s == NULL)
-				{
-					s = "(null)";
-				}
-				d = 0;
-				while (s[d] != '\0')
-				{
+				case 'c':
+					c = va_arg(ap, int);
+					number += write(1, &c, 1);
+					break;
 
-					number += write(1, &s[d], 1);
-					d++;
-				}
-			}
-			else if (format[i] == '%')
-			{
-				number += write(1, "%", 1);
-			}
-			else
-			{
-				number += write(1, "%", 1);
-				number += write(1, &format[i], 1);
+				case 's':
+			
+					s = va_arg(ap, char *);
+					if (s == NULL)
+					{
+						s = "(null)";
+					}
+					d = 0;
+					while (s[d] != '\0')
+					{
+
+						number += write(1, &s[d], 1);
+						d++;
+					}
+					break;
+
+				case '%':
+					number += write(1, "%", 1);
+					break;
+
+				case 'i':
+				case 'd':
+					integer = va_arg(ap, int);
+                    			if (integer < 0)
+                    			{
+                        			write(1, "-", 1);
+                        			number++;
+                        			integer = -integer;
+                    			}
+
+                    			div = 1;
+                    			while (integer / div >= 10)
+					{
+                        			div *= 10;
+					}
+                    			while (div > 0)
+                    			{
+                        			digit = integer / div + '0';
+                        			write(1, &digit, 1);
+                        			number++;
+                        			integer %= div;
+                        			div/= 10;
+                    			}
+                    			break;
+
+				default:
+					number += write(1, "Error: invalid format specifier\n", 35);;
+					exit(1);
 			}
 		}
 		else
